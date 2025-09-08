20ae31f4b10e3423c964fc56065aa052d0699e53
willianryan23 <regobemfica@gmail.com>
2025-09-08 13:48:12 -0300

    pri

---
+++ banco.php
@@ -0,0 +1,10 @@
+
+<?php
+
+$servidor = "localhost"; //servidor do banco de dados 127
+$usuario = "root"; //usuário do banco de dados
+$senha = "";
+$bd = "storegames";
+
+$conexao = mysqli_connect($servidor, $usuario, $senha, $bd);
+?>
